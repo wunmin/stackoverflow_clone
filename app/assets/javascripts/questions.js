e9071023f7888b9fe0ae0e79acb432eaf0d76d7c
Wunmin Wong <wunmin@Wunmins-MacBook-Air.local>
2015-06-23 10:20:37 +0800

    Add Font Awesome

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+	$(".question_upvote").on("click", function(event) {
+	event.preventDefault();
+	var questionId = $(this).data("question-id");
+	var url = "/questions/" + questionsId + "/vote"
+	$.ajax({
+	    type: "POST",
+	    url: url,
+	    data: {"question_id": questionId,
+	           "vote": 1},
+	    dataType: "json"
+	  }).done(function(response){
+	    $("#post" + response["post_id"] + " span").text(response["votes"] + " points");
+	    $("#post" + response["post_id"] + " button" + ".upvote").attr("disabled", "disabled")
+	});
+	});
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# 10.times do
+# 	User.create(
+# 		username: Faker::Internet.user_name,
+#     	password: Faker::Internet.password(4)
+#     )
+# end
+
+# 20.times do
+# 	Question.create(
+# 		title: Faker::Lorem.sentence,
+# 		description: Faker::Lorem.paragraphs,
+# 		user_id: User.pluck(:id).sample
+# 	)
+# end
+
+100.times do
+	Answer.create(
+		description: Faker::Lorem.paragraph,
+		user_id: User.pluck(:id).sample,
+		question_id: Question.pluck(:id).sample
+	)
+end
+
+# 20.times do
+# 	Response.create(
+# 		description: Faker::Lorem.sentence,
+# 		user_id: User.pluck(:id).sample,
+# 		responseable_id: Question.pluck(:id).sample,
+# 		responseable_type: "Question"
+# 	)
+# end
+
+# 20.times do
+# 	Response.create(
+# 		description: Faker::Lorem.sentence,
+# 		user_id: User.pluck(:id).sample,
+# 		responseable_id: Answer.pluck(:id).sample,
+# 		responseable_type: "Answer"
+# 	)
+# end
+
+
